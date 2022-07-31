8c926abdc1742c3f630ed72508c6fd4acd19dd9e
RainyMacondo <yaofanghao7@163.com>
2022-07-31 16:56:19 +0800

    2022.7.31

---
+++ OpenCV/fireDetection/cascade_data/cascade1.0/0、数据准备/裁剪出火焰部分.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+import pandas as pd
+import os
+import cv2
+
+base_dir = "./imgnight"  #要裁剪的图片的原文件夹位置
+
+output_dir = './imgnightcut/'  #裁剪后存放的文件夹位置
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+filelist = os.listdir(base_dir)
+
+i = 1
+for i in range(0, len(filelist)):
+
+    path = os.path.join(base_dir, filelist[i])
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+
+    # cropped = img[240:440, 550:780] #day
+    cropped = img[190:350, 300:500] #night
+
+    save_path = output_dir + str(i+1) + '.png'  #保存至另一文件夹
+    cv2.imwrite(save_path, cropped)
+
+    print('裁剪:%s' % (i+1) )
