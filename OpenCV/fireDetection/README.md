df07dbaa1a55f356687347f1ff4ac7b885e29f9c
RainyMacondo <yaofanghao7@163.com>
2022-11-23 22:12:59 +0800

    2022.11.23-

--- OpenCV/fireDetection/README.md
+++ OpenCV/fireDetection/README.md
@@ -1,19 +1,18 @@
-# 火焰检测 C++ 有关代码整理合集
-
+# fireDetection for C++ and RaspberryPi4B
 2022.7.1-
 
-## 代码说明
+## 简介
 ----------
 **常规方法**
-* fireDetection.cpp
-  * 对视频进行火焰检测
-* fireDetection-camera.cpp
-  * 对摄像头进行实时火焰检测
-* Simple-ubuntu-success
-  * 对摄像头进行实时火焰检测的虚拟机编译版本---可以使用
-* Simple-RasberryPi-not-test-yet
-  * 对摄像头进行实时火焰检测的树莓派编译版本---未测试能否使用
-
+* 火焰检测1.0版
+	* fireDetection.cpp
+		* 对视频进行火焰检测
+	* fireDetection-camera.cpp
+		* 对摄像头进行实时火焰检测
+* Simple-RasberryPi
+  * 对摄像头进行实时火焰检测的树莓派编译版本---可以使用
+* main.cpp
+	* 目前的最新版本
 ----------
 **级联分类器方法**
 * fireCascade.cpp 8.17更新
@@ -22,9 +21,111 @@
   * 分类器由多个简单分类器按一定的顺序级联而成。级联中的每个阶段根据小规模的特征子集取得特征值，并根据这个特征值快速地判断，决定拒绝或接受这个目标。如果每个阶段的判断都比上一个阶段更加精确，使性能得到提升（增强），那么整个级联分类器都会增强。这种做法的主要优势在于，级联中前面的阶段只进行一些简单的测试，可以快速排除掉那些明显不属于指定类别的实例。在早期阶段将它们排除掉后，通过扫描图像搜索某类物体时，大多数待测试的子窗口都将不属于指定类别，因而可以提高整个级联分类器的速度。这样，只有少数窗口需要经过全部阶段才能得到接受或排除的结论。
   * OpenCV提供了CascadeClassifier类，调用该类中的方法detectMultiScale，对其预测结果detections画矩形框即可
 
+## 部分代码说明
+* 初始化日志模块
+```cpp
+// log setting
+// https://github.com/amrayn/easyloggingpp
+el::Configurations defaultConf;
+defaultConf.setToDefault();
+el::Loggers::reconfigureLogger("default", defaultConf);
+el::Logger* defaultLogger = el::Loggers::getLogger("default");
+```
+* 轮廓提取，计算面积、周长、圆形度、偏心度特征
+```cpp
+vector<vector<Point>>contours;
+vector<Vec4i>hierarchy;
+cv::findContours(mask, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
+cv::drawContours(frame, contours, -1, Scalar(0, 0, 255), 3);
+int image_area = frame.rows * frame.cols;
+for (int i = 0; i < contours.size(); i++)
+{
+  double area = contourArea(contours[i]);
+  double length = arcLength(contours[i], true);
+  double roundIndex = 4 * 3.1415926 * area / (length * length + 0.00001);  // 圆形度
 
-## 文件夹说明
-* external_module
-  * 包含打印日志的模块easylogging++
-* fire_img
-  * 存放了级联分类器的模型和数据处理内容
+  if ((area > contours_ratio * image_area) && (roundIndex > round_low) 
+    && (length > cntlen_low) && (contours[i].size() > ellipse_low)){		
+    Rect rect = boundingRect(contours[i]);
+    rectangle(frame, rect, (255, 0, 0), 5);
+    
+    RotatedRect box = fitEllipse(contours[i]);
+    double ellipseA = box.size.height;
+    double ellipseB = box.size.width;
+    double eccIndex = sqrt(abs(pow(ellipseA, 2) - pow(ellipseB, 2))) / 2;  // 偏心度	
+    
+    string text = "Warning!";
+    cv::Point origin;
+    origin.x = frame.cols / 2;
+    origin.y = frame.rows / 2;
+    putText(frame, text, origin, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 1);
+  }
+```
+* 计算图像的熵值--2022.11.23
+```cpp
+//https://github.com/arnaudgelas/OpenCVExamples/blob/master/cvMat/Statistics/Entropy/Entropy.cpp
+cv::Scalar Entropy(cv::Mat image)
+{
+	std::vector<cv::Mat> channels;
+	cv::split(image, channels);
+	int histSize = 256;
+	float range[] = { 0, 256 };
+	const float* histRange = { range };
+	bool uniform = true;
+	bool accumulate = false;
+	cv::Mat hist0, hist1, hist2;
+	cv::calcHist(&channels[0], 1, 0, cv::Mat(), hist0, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&channels[1], 1, 0, cv::Mat(), hist1, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&channels[2], 1, 0, cv::Mat(), hist2, 1, &histSize, &histRange, uniform, accumulate);
+
+	//frequency
+	float f0 = 0, f1 = 0, f2 = 0;
+	for (int i = 0; i < histSize; i++){
+		f0 += hist0.at<float>(i);
+		f1 += hist1.at<float>(i);
+		f2 += hist2.at<float>(i);	
+	}
+
+	//entropy
+	cv::Scalar e;
+	e.val[0] = 0;
+	e.val[1] = 0;
+	e.val[2] = 0;
+	// e0=0, e1=0, e2=0;
+	float p0, p1, p2;
+
+	for (int i = 0; i < histSize; i++){
+		p0 = abs(hist0.at<float>(i)) / f0;
+		p1 = abs(hist1.at<float>(i)) / f1;
+		p2 = abs(hist2.at<float>(i)) / f2;
+		if (p0 != 0)
+			e.val[0] += -p0 * log10(p0);
+		if (p1 != 0)
+			e.val[1] += -p1 * log10(p1);
+		if (p2 != 0)
+			e.val[2] += -p2 * log10(p2);
+	}
+	return e;
+}
+```
+
+## 已完成部分说明
+* 实现了摄像头、视频、图像三种检测模式的选择
+* 实现了对火焰图像圆形度、偏心率、熵值等特征值的计算
+* 实现了将检测日志打印并保存到myeasylog.log
+  * 程序运行需包含external_module
+  * 包含打印日志的模块easylogging++库  
+* 实现了基于CppLinuxSerial库的串口通信  
+* 实现了将火焰特征值保存到csv/excel文件中
+  
+## 待完成内容
+* 修改为每隔flag帧处理一次图像，减少计算量
+* 预处理的自适应分割
+* C++编写SVM模型，建立火焰和其他干扰项特征值数据集，实现输入特征-->预测有火/无火
+
+## 参考资料
+* https://docs.opencv.org/4.x/df/d0d/tutorial_find_contours.html
+* https://github.com/amrayn/easyloggingpp
+* https://github.com/gbmhunter/CppLinuxSerial.git
+* https://github.com/arnaudgelas/OpenCVExamples/blob/master/cvMat/Statistics/Entropy/Entropy.cpp
+* https://www.csie.ntu.edu.tw/~cjlin/libsvm/
--- OpenCV/fireDetection/main.cpp
+++ OpenCV/fireDetection/main.cpp
@@ -33,151 +33,9 @@ double contours_ratio = 0;
 double round_low = 0.1;
 int cntlen_low = 20;
 
-Mat imgopen(Mat mask, int kernal_size);
-void processing(Mat frame);
-
 static void help(char* progName) {
-	cout << endl
-		<< "Usage:" << endl
-		<< progName << "[mode - c / v / i ]" << "[save_path -- default out.mp4]" << endl;
-}
-
-int main(int argc, char** argv)
-{
-	help(argv[0]);
-
-	// log setting
-	// https://github.com/amrayn/easyloggingpp
-	el::Configurations defaultConf;
-	defaultConf.setToDefault();
-	el::Loggers::reconfigureLogger("default", defaultConf);
-	el::Logger* defaultLogger = el::Loggers::getLogger("default");
-
-	//if (argc < 2) {
-	//	cout << "please enter mode" 
-	//		<< " [mode -- c / v]" 			
-	//		<< endl;
-	//	return -1;
-	//}
-
-	LOG(INFO) << "Start fire detect!";
-	char answer = 0;
-	cout << "please enter the detect mode [c -camera / v -video / i -image]" << endl;
-	cin >> answer;
-	switch (answer)
-	{
-	// camera detection
-	case 'c':
-	{
-		VideoCapture capture;
-		capture.open(0); // choose camera index - 1 for usb camera
-		capture.set(CAP_PROP_FRAME_WIDTH, CapWidth);
-		capture.set(CAP_PROP_FRAME_HEIGHT, CapHeight);
-		if (!capture.isOpened()) {
-			cerr << "camera not open!" << endl;
-			return -1;
-		}
-		LOG(INFO) << "fps:" << fps;
-		LOG(INFO) << "width:" << int(capture.get(CAP_PROP_FRAME_WIDTH));
-		LOG(INFO) << "height:" << int(capture.get(CAP_PROP_FRAME_HEIGHT));
-		VideoWriter writer;
-		int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
-		Size size = Size(int(capture.get(CAP_PROP_FRAME_WIDTH)), int(capture.get(CAP_PROP_FRAME_HEIGHT)));
-		string save_path = "out.avi";
-		writer.open(save_path, codec, fps, size, true);
-
-		while (1)
-		{
-			Mat frame;
-			capture >> frame;
-			if (frame.empty())
-				break;
-
-			processing(frame);
-
-			writer.write(frame);
-			if (!writer.isOpened()) {
-				cerr << "failed to open the video" << endl;
-				return -1;
-			}
-			if (!capture.read(frame)) {
-				cout << "detection done!" << endl;
-				break;
-			}
-			int c = waitKey(50);
-			if (c == 27) break;
-		}
-		writer.release();
-		return 0;
-	}
-
-	// video detection		
-	case 'v':
-	{
-		VideoCapture capture("day.mp4"); // day.mp4 as example
-		VideoWriter writer;
-		int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
-		Size size = Size(int(capture.get(CAP_PROP_FRAME_WIDTH)), int(capture.get(CAP_PROP_FRAME_HEIGHT)));
-		string save_path = "out.avi";
-		writer.open(save_path, codec, fps, size, true);
-		Size dsize = Size(800, 450); // resize image for processing faster
-		while (1)
-		{
-			Mat frame;
-			capture >> frame;
-			if (frame.empty())
-				break;
-
-			resize(frame, frame, dsize, 0, 0, INTER_AREA);
-			processing(frame);
-
-			writer.write(frame);
-			if (!writer.isOpened()) {
-				cout << "failed to open the video" << endl;
-				return -1;
-			}
-			if (!capture.read(frame)) {
-				cout << "detection done!" << endl;
-				break;
-			}
-			int c = waitKey(50);
-			if (c == 27) break;
-		}
-		writer.release();
-		return 0;
-	}
-
-	// image detection
-	case 'i':
-	{
-		Mat img = cv::imread("1.jpg");
-		processing(img);
-		cv:imwrite("result.jpg", img);
-
-		int x;
-		cout << "enter 1 to exit:" ;
-		cin >> x;
-		if (x == 1){
-			cout << "detection done!" << endl;
-			return 0;
-		}
-	}
-
-	// more functions : cascade classification -- see in fireCascade.cpp
-	case 'e': 
-	{
-		cerr << "Using opencv-cascade detection method. Not finished yet." << endl;
-		return -1;
-	}
-
-	default: 
-	{
-		cerr << "please enter the correct mode [c / v / i]" << endl;
-		return -1;
-	}
-	
-	}
-	return 0;
+	cout << "Usage:" << endl
+		 << progName << "[mode - c / v / i ]" << "[save_path -- default out.mp4]" << endl;
 }
 
 Mat imgopen(Mat mask, int kernal_size)
@@ -188,19 +46,69 @@ Mat imgopen(Mat mask, int kernal_size)
 	return maskDst;
 }
 
-void processing(Mat frame)
+// caculate entropy for a gray image
+//https://github.com/arnaudgelas/OpenCVExamples/blob/master/cvMat/Statistics/Entropy/Entropy.cpp
+cv::Scalar Entropy(cv::Mat image)
+{
+	std::vector<cv::Mat> channels;
+	cv::split(image, channels);
+	int histSize = 256;
+	float range[] = { 0, 256 };
+	const float* histRange = { range };
+	bool uniform = true;
+	bool accumulate = false;
+	cv::Mat hist0, hist1, hist2;
+	cv::calcHist(&channels[0], 1, 0, cv::Mat(), hist0, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&channels[1], 1, 0, cv::Mat(), hist1, 1, &histSize, &histRange, uniform, accumulate);
+	cv::calcHist(&channels[2], 1, 0, cv::Mat(), hist2, 1, &histSize, &histRange, uniform, accumulate);
+
+	//frequency
+	float f0 = 0, f1 = 0, f2 = 0;
+	for (int i = 0; i < histSize; i++){
+		f0 += hist0.at<float>(i);
+		f1 += hist1.at<float>(i);
+		f2 += hist2.at<float>(i);	
+	}
+
+	//entropy
+	cv::Scalar e;
+	e.val[0] = 0;
+	e.val[1] = 0;
+	e.val[2] = 0;
+	// e0=0, e1=0, e2=0;
+	float p0, p1, p2;
+
+	for (int i = 0; i < histSize; i++){
+		p0 = abs(hist0.at<float>(i)) / f0;
+		p1 = abs(hist1.at<float>(i)) / f1;
+		p2 = abs(hist2.at<float>(i)) / f2;
+		if (p0 != 0)
+			e.val[0] += -p0 * log10(p0);
+		if (p1 != 0)
+			e.val[1] += -p1 * log10(p1);
+		if (p2 != 0)
+			e.val[2] += -p2 * log10(p2);
+	}
+	return e;
+}
+
+// main processing program for image
+void processing(Mat frame, ofstream &oFile)
 {
 	Mat img1, hsv, mask;
 	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	
+	//entropy -2022.11.23
+	cv::Scalar ent = Entropy(frame);
+
 	dilate(frame, img1, kernel);
 	cv::cvtColor(img1, hsv, CV_BGR2HSV);
 	Scalar lower(hl, sl, vl);
 	Scalar upper(hh, sh, vh);
 	cv::inRange(hsv, lower, upper, mask);
-
 	mask = imgopen(mask, kernal_size);
-	cv::namedWindow("show_hsv", WINDOW_NORMAL);
-	cv::imshow("show_hsv", mask);
+	//cv::namedWindow("show_hsv", WINDOW_NORMAL);
+	//cv::imshow("show_hsv", mask);
 
 	vector<vector<Point>>contours;
 	vector<Vec4i>hierarchy;
@@ -214,23 +122,28 @@ void processing(Mat frame)
 		double roundIndex = 4 * 3.1415926 * area / (length * length + 0.00001);
 
 		if ((area > contours_ratio * image_area) && (roundIndex > round_low) 
-			&& (length > cntlen_low) && (contours[i].size() > ellipse_low))
-		{		
+			&& (length > cntlen_low) && (contours[i].size() > ellipse_low)){		
 			Rect rect = boundingRect(contours[i]);
 			rectangle(frame, rect, (255, 0, 0), 5);
 			
 			RotatedRect box = fitEllipse(contours[i]);
 			double ellipseA = box.size.height;
 			double ellipseB = box.size.width;
-			double eccIndex = sqrt(abs(pow(ellipseA, 2) - pow(ellipseB, 2))) / 2;  // ¼ÆËãÆ«ÐÄ¶È	
+			double eccIndex = sqrt(abs(pow(ellipseA, 2) - pow(ellipseB, 2))) / 2;  // 偏心度	
 			
 			string text = "Warning!";
 			cv::Point origin;
 			origin.x = frame.cols / 2;
 			origin.y = frame.rows / 2;
 			putText(frame, text, origin, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 1);
-
-			LOG(INFO) << "Find fire." << "-area:" << area << "-length:" << length << "-roundIndex:" << roundIndex << "-eccIndex:" << eccIndex;
+			//putText(frame, std::to_string(ent.val[0]), origin, FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 0, 0), 1);
+			
+			//write data to excel
+			oFile << area << "," << length << "," << roundIndex << "," 
+				<< eccIndex << "," << ent.val[0] << endl;
+			LOG(INFO) << "Find fire." << "-area:" << area 
+				<< "-length:" << length << "-roundIndex:" << roundIndex 
+				<< "-eccIndex:" << eccIndex << "-entropy:" << ent.val[0];
 		}
 		//else {
 		//	LOG(INFO) << "No fire.";
@@ -238,4 +151,136 @@ void processing(Mat frame)
 	}
 	cv::namedWindow("result", WINDOW_NORMAL);
 	cv::imshow("result", frame);
+}
+
+int main(int argc, char** argv)
+{
+	help(argv[0]);
+
+	// log setting: write to myeasylog.log
+	// https://github.com/amrayn/easyloggingpp
+	el::Configurations defaultConf;
+	defaultConf.setToDefault();
+	el::Loggers::reconfigureLogger("default", defaultConf);
+	el::Logger* defaultLogger = el::Loggers::getLogger("default");
+
+	// ofstream setting: write data to excel
+	ofstream oFile;
+	oFile.open("detect_result.csv", ios::out | ios::trunc);
+	oFile << "-area:" << "," << "-length:" << ","
+		<< "-roundIndex:" << "," << "-eccIndex:" << ","
+		<< "-entropy:" << endl;
+
+	LOG(INFO) << "Start fire detect!";
+	LOG(INFO) << "Detect result save to detect_result.csv";
+	char answer = 0;
+	cout << "please enter the detect mode [c -camera / v -video / i -image]" << endl;
+	cin >> answer;
+	switch (answer)
+	{
+	// camera detection
+	case 'c':{
+		VideoCapture capture;
+		capture.open(0); // choose camera index - 1 for usb camera
+		capture.set(CAP_PROP_FRAME_WIDTH, CapWidth);
+		capture.set(CAP_PROP_FRAME_HEIGHT, CapHeight);
+		if (!capture.isOpened()){
+			cerr << "camera not open!" << endl;
+			return -1;
+		}
+		LOG(INFO) << "fps:" << fps;
+		LOG(INFO) << "width:" << int(capture.get(CAP_PROP_FRAME_WIDTH));
+		LOG(INFO) << "height:" << int(capture.get(CAP_PROP_FRAME_HEIGHT));
+		VideoWriter writer;
+		int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
+		Size size = Size(int(capture.get(CAP_PROP_FRAME_WIDTH)), int(capture.get(CAP_PROP_FRAME_HEIGHT)));
+		string save_path = "out.avi";
+		writer.open(save_path, codec, fps, size, true);
+
+		while (1){
+			Mat frame;
+			capture >> frame;
+			if (frame.empty())
+				break;
+
+			processing(frame, oFile);
+
+			writer.write(frame);
+			if (!writer.isOpened()){
+				cerr << "failed to open the video" << endl;
+				return -1;
+			}
+			if (!capture.read(frame)){
+				cout << "detection done!" << endl;
+				break;
+			}
+			int c = waitKey(50);
+			if (c == 27) break;
+		}
+		writer.release();
+		return 0;
+	}
+
+	// video detection		
+	case 'v':{
+		VideoCapture capture("day.mp4"); // day.mp4 as example
+		VideoWriter writer;
+		int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');
+		Size size = Size(int(capture.get(CAP_PROP_FRAME_WIDTH)), int(capture.get(CAP_PROP_FRAME_HEIGHT)));
+		string save_path = "out.avi";
+		writer.open(save_path, codec, fps, size, true);
+		Size dsize = Size(800, 450); // resize image for processing faster
+		while (1)
+		{
+			Mat frame;
+			capture >> frame;
+			if (frame.empty())
+				break;
+			resize(frame, frame, dsize, 0, 0, INTER_AREA);
+			processing(frame, oFile);
+			writer.write(frame);
+			if (!writer.isOpened()){
+				cout << "failed to open the video" << endl;
+				return -1;
+			}
+			if (!capture.read(frame)){
+				cout << "detection done!" << endl;
+				break;
+			}
+			int c = waitKey(50);
+			if (c == 27) break;
+		}
+		writer.release();
+		return 0;
+	}
+
+	// image detection
+	case 'i':{
+		Mat img = cv::imread("1.jpg");
+		processing(img, oFile);
+		//cv:imwrite("result.jpg", img);
+
+		int x;
+		cout << "enter 1 to exit:" ;
+		cin >> x;
+		if (x == 1){
+			cout << "detection done!" << endl;
+			return 0;
+		}
+	}
+
+	// more functions : cascade classification -- see details in fireCascade.cpp
+	case 'e':{
+		cerr << "Using opencv-cascade detection method. Not finished yet." << endl;
+		return -1;
+	}
+
+	default:{
+		cerr << "please enter the correct mode [c / v / i]" << endl;
+		return -1;
+	}	
+	
+	}
+	oFile.close();
+	return 0;
 }
