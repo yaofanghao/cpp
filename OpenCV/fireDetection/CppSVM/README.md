90f1d931ddb181d1868483a4bf703e2fc4b75d43
RainyMacondo <yaofanghao7@163.com>
2022-11-26 11:39:15 +0800

    2022.11.26

---
+++ OpenCV/fireDetection/CppSVM/README.md
@@ -0,0 +1,82 @@
+# 基于C++的SVM模型的简单实现
+* 2022.11.25起-
+* 本项目是简单的二分类问题，输入火焰的红外、紫外、图像等多种融合特征，**输出0或1（表示无/有火）**
+  
+## 参考资料：
+* https://www.csie.ntu.edu.tw/~cjlin/libsvm/
+* https://blog.csdn.net/jsgaobiao/article/details/50202757
+
+## 实现过程
+* 实现在main.cpp，编译**需包含svm.cpp和svm.h文件**
+* 数据集
+  * train.txt 训练集和验证集
+  * eval.txt 测试集
+  * real_value.txt 测试集的真实值
+* 数据清理
+  * 避免各类特征值权重对模型的影响，以及防止训练时数值计算的困难，将输入特征统一**归一化**到[0,1]之间
+* 调用方法和参数说明--不清楚的设置参数先选择默认值
+  * **svm_model 为模型类**，通过训练或加载训练好的模型文件获得
+  * **svm_parameter 为参数类**，主要为支持向量机设定参数，具体参数如下：
+    ```cpp
+    svm_parameter.svm_type
+    svm类型：SVM设置类型(默认svm_parameter.C_SVC)
+    svm_parameter.C_SVC -- C-SVC
+    svm_parameter.NU_SVC -- ν-SVC
+    svm_parameter.ONE_CLASS – 一类SVM
+    svm_parameter.EPSILON_SVR -- ε -SVR
+    svm_parameter.NU_SVR -- ν-SVR
+
+    svm_parameter.kernel_type
+    核函数类型：核函数设置类型(svm_parameter.LINEAR)
+    svm_parameter.LINEAR – 线性：u'×v
+    svm_parameter.POLY– 多项式：(γ×u'×v + coef0)^degree
+    svm_parameter.RBF – RBF函数：exp(-γ×|u-v|^2)
+    svm_parameter.SIGMOID – sigmoid：tanh(γ×u'×v + coef0)
+
+    svm_parameter.degree
+    核函数中的degree设置(默认3)
+
+    svm_parameter.coef0
+    核函数中的coef0设置(默认0)
+
+    svm_parameter.shrinking
+    是否使用启发式，0或1(默认1)
+
+    svm_parameter.nu
+    设置ν-SVC，一类SVM和ν- SVR的参数(默认0.5)
+
+    svm_parameter.C
+    设置C-SVC，ε -SVR和ν-SVR的参数(默认1)
+
+    svm_parameter.cache_size
+    设置cache内存大小，以MB为单位(默认40)
+    ```
+  * svm_problem 相当于训练集合，可讲需要训练的数据加入该类传递给训练器
+* 其他说明
+  * 参数prob是svm_problem类型数据，具体结构定义如下：   
+    ```cpp
+    struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
+
+    struct svm_problem //存储本次参加运算的所有样本(数据集)，及其所属类别。
+    {
+    int n; //记录样本总数
+    double *y; //指向样本所属类别的数组
+    struct svm_node **x; //指向一个存储内容为指针的数组
+    };
+    其中svm_node的结构体定义如下：
+    struct svm_node //用来存储输入空间中的单个特征
+    {
+    int index; //输入空间序号，假设输入空间数为m
+    double value; //该输入空间的值
+    };
+    ```
+  ![svm_train](svm_train.jpg "svm_train函数说明") 
+* 两个核心函数
+  * **struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param)**。设定好参数之后，运行改函数，该函数返回一个struct svm_model *SVM模型的指针，可以使用svm_save_model(const char *model_file_name, const struct svm_model *model)函数，把这个模型保存在磁盘中。
+  * **double svm_predict(const struct svm_model *model, const struct svm_node *x)**。参数model，是一个SVM模型的指针，可以使用函数struct svm_model *svm_load_model(const char *model_file_name)，导入训练时保存好的SVM模型，此函数返回一个SVM模型的指针，可以直接赋值给变量model。该函数返回一个double类型，指明被预测数据属于哪个类。面对两分类问题的时候，**通常使用+1代表正样本，即类1；-1代表负样本，即类2**。根据返回的double值就可以知道预测数据的类别。
+* 查看模型
+  * 模型信息保存至model.txt
+* 补充资料
+  * SVM是一个二分类器，当遇到多类别的时候，一般采取如下两种策略。 
+  a.一对多法（one-versus-rest,简称1-v-r SVMs）。训练时依次把某个类别的样本归为一类,其他剩余的样本归为另一类，这样k个类别的样本就构造出了k个SVM。分类时将未知样本分类为具有最大分类函数值的那类。
+  b.一对一法（one-versus-one,简称1-v-1 SVMs）。其做法是在任意两类样本之间设计一个SVM，因此k个类别的样本就需要设计k(k-1)/2个SVM。当对一个未知样本进行分类时，最后得 票最多的类别即为该未知样本的类别。Libsvm中的多类分类就是根据这个方法实现的
---
+++ OpenCV/fireDetection/CppSVM/main.cpp
@@ -0,0 +1,211 @@
+#include "svm.h"
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <ctime>
+using namespace std;
+
+#ifdef WIN32
+#pragma warning (disable: 4514 4786)
+#endif
+
+svm_parameter param;
+svm_problem prob;
+svm_model *svmModel;
+list<svm_node*> xList;
+list<double>  yList ;
+const int MAX=10;
+const int nTstTimes=10;
+vector<int> predictvalue;
+vector<int> realvalue;
+int trainNum=0;
+
+// -------setting----------
+int class_num = 85;  // class nums of input
+
+void setParam()
+{
+    param.svm_type = C_SVC;  // type of RBF
+	param.kernel_type = RBF;  // RBF kernal function exp(-γ×|u-v|^2)
+	param.degree = 3;  // params of kernal function
+	param.gamma = 0.5;  
+	param.coef0 = 0;
+	param.nu = 0.5;  
+	param.cache_size = 40;  // cache size of program (MB)
+	param.C = 500;
+	param.eps = 1e-3;
+	param.p = 0.1;
+	param.shrinking = 1;
+	// param.probability = 0;
+	param.nr_weight = 0;
+	param.weight = NULL;
+    param.weight_label =NULL;
+}
+
+void train(char *filePath)
+{	
+	FILE *fp;
+	int k;
+	int line=0;
+	int temp;
+ 
+	if((fp=fopen(filePath,"rt"))==NULL)
+		return ;
+	while(1)
+	{
+		 svm_node* features = new svm_node[class_num+1];
+		 
+		 for(k=0;k<class_num;k++)
+		 {
+		 	fscanf(fp,"%d",&temp); 	         
+			features[k].index = k + 1;
+			features[k].value = temp/(MAX*1.0) ;
+		}			
+		features[class_num].index = -1;
+
+		fscanf(fp,"%d",&temp);
+		xList.push_back(features);
+		yList.push_back(temp);
+    
+		line++;
+		trainNum=line;
+		if(feof(fp)) 
+			break; 
+	}
+   
+    setParam();
+	prob.l=line;
+	prob.x=new svm_node *[prob.l];  // 对应的特征向量
+	prob.y = new double[prob.l];    // 放的是值
+	int index=0;	
+	while(!xList.empty())
+	{
+		prob.x[index]=xList.front();
+		prob.y[index]=yList.front();
+		xList.pop_front();
+		yList.pop_front();
+		index++;
+	}
+	//std::cout<<prob.l<<"list end\n";
+	svmModel=svm_train(&prob, &param);
+
+	//std::cout<<"\n"<<"over\n";
+	//保存model
+	svm_save_model("model.txt",svmModel);
+
+	//释放空间
+	delete  prob.y;
+	delete [] prob.x;
+	svm_free_and_destroy_model(&svmModel);
+}
+
+void predict(char *filePath)
+{
+    svm_model *svmModel = svm_load_model("model.txt");
+   	FILE *fp;
+	int line=0;
+	int temp;
+
+	if((fp=fopen(filePath,"rt"))==NULL)
+		return ;
+	
+	while(1)
+	{
+		 svm_node* input = new svm_node[class_num+1];
+		 for(int k=0;k<class_num;k++)
+		 {
+		 	fscanf(fp,"%d",&temp);
+			input[k].index = k + 1;
+			input[k].value = temp/(MAX*1.0);
+		}
+		input[class_num].index = -1;
+		
+    	int predictValue=svm_predict(svmModel, input);
+		predictvalue.push_back(predictValue);
+
+		cout<<predictValue<<endl;
+		if(feof(fp)) 
+			break; 
+	}
+}
+
+void writeValue(vector<int> &v,string filePath)
+{
+   	FILE *pfile=fopen("filePath","wb");
+
+	vector<int>::iterator iter=v.begin();
+	char *c=new char[2];
+	for(;iter!=v.end();++iter)
+	{		
+	    c[1]='\n';
+		
+		if(*iter==0)
+		   c[0]='0';
+		else
+			c[0]='1';
+       fwrite(c,1,2,pfile);
+	}
+	fclose(pfile);
+    delete c;
+}
+
+bool getRealValue()
+{
+    FILE *fp;
+	int temp;
+
+	if((fp=fopen("real_value.txt","rt"))==NULL)
+		return false;
+	while(1)
+	{
+		
+		fscanf(fp,"%d",&temp);
+		realvalue.push_back(temp);  
+		if(feof(fp)) 
+			break; 
+	}
+	return true;
+}
+
+double getAccuracy()
+{
+    if(!getRealValue())
+		return 0.0;
+	int counter=0;
+	int counter1=0;
+	for(int i=0;i<realvalue.size();i++)
+	{
+		if(realvalue.at(i)==predictvalue.at(i))
+		{
+			counter++;       //测试正确的个数
+	    	if(realvalue.at(i)==1)
+			  counter1++;
+		}
+	}
+    //cout<<realvalue.size()<<endl;  //目标值为1的记录测试真确的个数
+	return counter*1.0/realvalue.size();
+}
+
+int main()
+{
+    clock_t t1,t2,t3;
+	
+	cout<<"请稍等待..."<<endl;
+	t1=clock();
+	train((char*)"train.txt");   // train
+    t2=clock();
+	
+    predict((char*)"eval.txt");        // predict
+	t3=clock();
+	writeValue(predictvalue,"result.txt");  // write predict value to result.txt
+	double accuracy=getAccuracy();          // get accuracy of predict
+	cout<<"训练数据共:"<<trainNum<<"条记录"<<endl;
+	cout<<"测试数据共:"<<realvalue.size()<<"条记录"<<endl;
+	cout<<"训练的时间:"<<1.0*(t2-t1)/nTstTimes<<"ms"<<endl;
+	cout<<"预测的时间:"<<1.0*(t3-t2)/nTstTimes<<"ms"<<endl;
+    cout<<"测试正确率为:"<<accuracy*100<<"%"<<endl;    
+
+	return 0;
+}
