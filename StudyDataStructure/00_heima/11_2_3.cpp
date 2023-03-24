a835d538103b419e6d9d61c1a2f51f86cd13ccc1
yaofanghao <yaofanghao7@163.com>
2023-03-24 10:19:19 +0800

    2023.3.24 update 2023.3-C-C++-笔记.md

--- StudyDataStructure/00_heima/11_2_3.cpp
+++ StudyDataStructure/00_heima/11_2_3.cpp
@@ -1,6 +1,6 @@
-// p182-184 类模板案例：数组类封装
-// 实现通用数组
-// 需求分析见MyArray.hpp
+// p182-184 ????尸????????????
+// ??????????
+// ?????????MyArray.hpp
 #include <iostream>
 using namespace std;
 #include <string>
@@ -15,29 +15,29 @@ void printArray(MyArray<int>& arr) {
 void test01() {
 	MyArray<int>arr(5);
 
-	//利用好尾插法
+	//???ú?β?巨
 	for (int i = 0; i < 5; i++) {
 		arr.Push_Back(i);
 	}
 
-	cout << "arr的打印输出为：" << endl;
+	cout << "arr??????????" << endl;
 	
 	printArray(arr);
 
-	cout << "arr的容量：" << arr.GetCapacity() << endl;
-	cout << "arr的大小：" << arr.GetSize() << endl;
+	cout << "arr????????" << arr.GetCapacity() << endl;
+	cout << "arr???С??" << arr.GetSize() << endl;
 
 	MyArray <int>arr1(arr);
 	printArray(arr1);
-	//尾删
+	//β?
 	arr1.Pop_Back();
-	cout << "arr1尾删后" << endl;
-	cout << "arr1的容量：" << arr1.GetCapacity() << endl;
-	cout << "arr1的大小：" << arr1.GetSize() << endl;
+	cout << "arr1β???" << endl;
+	cout << "arr1????????" << arr1.GetCapacity() << endl;
+	cout << "arr1???С??" << arr1.GetSize() << endl;
 
 }
 
-//测试自定义数据类型
+//?????????????????
 class Person {
 public:
 	Person(){}
@@ -52,25 +52,25 @@ public:
 
 void PrintPersonArray(MyArray<Person>& arr) {
 	for (int i = 0; i < arr.GetSize(); i++) {
-		cout << "姓名：" << arr[i].m_name << "\t年龄：" << arr[i].m_age << endl;
+		cout << "??????" << arr[i].m_name << "\t????" << arr[i].m_age << endl;
 	}
 }
 
 void test02() {
 	MyArray<Person>arr(10);
 
-	Person p1("孙悟空", 555);
-	Person p2("红孩儿", 555);
-	Person p3("猪八戒", 555);
-	Person p4("唐三藏", 555);
-	Person p5("沙悟净", 555);
-	Person p6("白骨精", 555);
-	Person p7("女儿国", 555);
-	Person p8("白龙马", 555);
-	Person p9("史蒂夫", 555);
-	Person p10("甘道夫", 555);
+	Person p1("?????", 555);
+	Person p2("????", 555);
+	Person p3("?????", 555);
+	Person p4("??????", 555);
+	Person p5("???", 555);
+	Person p6("????", 555);
+	Person p7("?????", 555);
+	Person p8("??????", 555);
+	Person p9("????", 555);
+	Person p10("?????", 555);
 
-	//将数据插入数组
+	//?????????????
 	arr.Push_Back(p1);
 	arr.Push_Back(p2);
 	arr.Push_Back(p3);
@@ -83,8 +83,8 @@ void test02() {
 	arr.Push_Back(p10);
 
 	PrintPersonArray(arr);
-	cout << "arr的容量：" << arr.GetCapacity() << endl;
-	cout << "arr的大小：" << arr.GetSize() << endl;
+	cout << "arr????????" << arr.GetCapacity() << endl;
+	cout << "arr???С??" << arr.GetSize() << endl;
 
 }
 
